f53dafaf6249ff8abdfbd873bf5b1d6f988d2ad0
unknown <carlos.goncalves1193@gmail.com>
2024-10-19 14:57:01 -0300

    Change user name structure in database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,7 +13,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'password' => static::$password ??= Hash::make('password')
         ];
--- database/schema.md
+++ database/schema.md
@@ -2,7 +2,8 @@
 
 Table users{
   id integer
-  name varchar(255)
+  first_name varchar(255)
+  last_name varchar(255)
   email varchar(255)
   password varchar(255)
   created_at timestamp
